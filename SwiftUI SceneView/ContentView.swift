6650a85389f43eaa9c2f9769c6118ff9fa52b923
Kyle <kylei.tw@gmail.com>
2024-02-01 16:00:56 +0800

    Initial Commit

---
+++ SwiftUI SceneView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI SceneView
+//
+//  Created by admin on 2024/2/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI SceneView/SwiftUI_SceneViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SceneViewApp.swift
+//  SwiftUI SceneView
+//
+//  Created by admin on 2024/2/1.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SceneViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
